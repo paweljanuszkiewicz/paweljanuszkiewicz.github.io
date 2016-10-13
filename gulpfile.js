ab14e0500688df6d2082dde44dbb1513d4e1959e
paweljanuszkiewicz <paweljanuszkiewicz@outlook.com>
2016-10-13 20:50:36 +0200

    finished mobile menu

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    pug = require('gulp-pug'),
+    prefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
+
+function errorLog (error) {
+  console.error.bind(error);
+  this.emit('end');
+}
+gulp.task('sass', function () {
+  return sass('sass/*.sass')
+    .on('error', errorLog)
+    .pipe(prefix('last 4 versions'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function () {
+  return gulp.src('*.pug')
+    .pipe(pug())
+    .on('error', errorLog)
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src('js/*.js')
+    .pipe(browserSync.stream());
+});
+
+// Static Server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: ".",
+        open: false
+    });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('sass/*.sass', ['sass']);
+  gulp.watch('*.pug', ['pug']);
+  gulp.watch('js/*.js', ['js']);
+});
+
+gulp.task('default', ['sass', 'pug', 'js', 'serve', 'watch']);
---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+      anchors:['start', 'about', 'skills', 'js', 'psd', 'contact'],
+      afterLoad: function(anchor, index){
+        var $activeItem;
+        $activeItem = $('nav').find('a[href="#' + anchor + '"]');
+        $activeItem
+            .addClass('active')
+            .parent()
+            .siblings()
+            .children().removeClass('active');
+    }
+  });
+
+  $('nav a').on('click', function () {
+    $(this).addClass('active');
+  });
+
+  $('.hamburger').on('click', function () {
+    if ($(this).hasClass('active'))
+      $(this).menuHide();
+    else
+      $(this).menuShow();
+  });
+
+  $.fn.menuShow = function () {
+    var $nav = $(this).parent().parent('aside').find('nav');
+    $nav.addClass('active');
+    $nav.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+      $nav.find('li').each(function (index) {
+        $(this).css('transition-delay', index * 100 + 'ms');
+        $(this).addClass('show');
+      });
+    });
+    $(this).addClass('active');
+  }
+  $.fn.menuHide = function () {
+    var $nav = $(this).parent().parent('aside').find('nav');
+    $nav.removeClass('active');
+    $(this).removeClass('active');
+    $nav.find('li').css('transition-delay', 0 + 'ms').removeClass('show');
+  }
+});
