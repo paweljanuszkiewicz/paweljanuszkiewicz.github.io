3f7a5ba145a361edd2d06bab44d8381c24fdef29
paweljanuszkiewicz <paweljanuszkiewicz@outlook.com>
2016-10-19 20:57:49 +0200

    fs 24

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   console.log($(window).width());
-  if ($(window).width() > 1200) {
+  if ($(window).width() > 1200 && $(window).height() > 880) {
     $('#fullpage').fullpage({
       anchors: ['start', 'about', 'skills', 'js', 'psd', 'contact'],
       verticalCentered: false,
